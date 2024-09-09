d8c014062d5b8fab3c6061d66980b752cf9dc55d
Tinotenda muchenje <tinomu@imgsol.co.zw>
2024-09-09 10:28:52 +0200

    changed model to lower and cheaper

--- part1.ts
+++ part1.ts
@@ -3,7 +3,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 async function main() {
-  const chatModel = new ChatOpenAI({ model: "gpt-4" });
+  const chatModel = new ChatOpenAI({ model: "gpt-4o-mini" });
 
   const response = await chatModel.invoke("What is Hello World?");
   console.log(response);
