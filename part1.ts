92a1617da9ae301e8eebe2abc0d50bbd2b456906
Tinotenda muchenje <tinomu@imgsol.co.zw>
2024-09-09 10:24:28 +0200

    Fixed missing model

--- part1.ts
+++ part1.ts
@@ -1,7 +1,9 @@
 import { ChatOpenAI } from "@langchain/openai";
+import dotenv from 'dotenv';
+dotenv.config();
 
 async function main() {
-  const chatModel = new ChatOpenAI({});
+  const chatModel = new ChatOpenAI({ model: "gpt-4" });
 
   const response = await chatModel.invoke("What is Hello World?");
   console.log(response);
